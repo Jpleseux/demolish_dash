905e683bf8f4155d1edaec899324005baa66e3d4
Jpleseux <joaopleseux@gmail.com>
2025-11-27 15:06:00 -0400

    add modify

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'https://demolish.lseux.dev.br',
     methods: ['GET', 'POST']
   }
 });
